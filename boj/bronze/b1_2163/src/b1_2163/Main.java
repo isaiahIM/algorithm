ad77e101a83b7b305b2b75d020fb88c6fce0c064
isaiah im <isaiahim0214@gmail.com>
2023-11-06 14:49:34 +0900

    [boj]2163 update

---
+++ boj/bronze/b1_2163/src/b1_2163/Main.java
@@ -0,0 +1,15 @@
+package b1_2163;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        int M, N;
+        Scanner sc = new Scanner(System.in);
+
+        M=sc.nextInt();
+        N=sc.nextInt();
+
+        System.out.println(M*N-1);
+    }
+}
