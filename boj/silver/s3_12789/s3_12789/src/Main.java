a89275857ef0cbfd3dcd366173e77088c9969d90
Isaiah IM <isaiahim0214@gmail.com>
2024-01-28 22:13:49 +0900

    [boj] 12789update

---
+++ boj/silver/s3_12789/s3_12789/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        int n;
+        int []line;
+        int i;
+        int order=1;
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        line = new int[n];
+
+        for(i=0; i<n; i++){
+            line[i] = sc.nextInt();
+        }
+
+        for(i=0; i<n; i++){
+
+            if(line[i]!=order){
+                stack.push(line[i]);
+            } else{
+                order++;
+            }
+
+            while(!stack.isEmpty() && stack.peek()==order){
+                order++;
+                stack.pop();
+            }
+
+        }
+
+        if(order==n+1){
+            System.out.println("Nice");
+        } else{
+            System.out.println("Sad");
+        }
+
+    }
+}
